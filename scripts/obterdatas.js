12b383a54f20f39376b44bca53206cf03f19c8b4
Lucas da Luz Morais <llucaslluz@gmail.com>
2026-07-04 13:24:18 -0300

    At1 - Semana1

---
+++ scripts/obterdatas.js
@@ -0,0 +1,5 @@
+const anoAtual = document.querySelector("#anoatual");
+const ultimaModificacao = document.querySelector("#ultimaModificacao");
+
+anoAtual.textContent = new Date().getFullYear();
+ultimaModificacao.textContent = `Última modificação: ${document.lastModified}`;
